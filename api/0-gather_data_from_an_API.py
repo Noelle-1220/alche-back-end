f433f6f1c30a874923e7240ea106c69690d39538
Noelle-1220 <m.ingabire3@alustudent.com>
2026-08-19 17:13:59 +0400

    test

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Gather data from an API and display employee TODO list progress."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Get employee name
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
+    employee_name = user.get("name")
+
+    # Get employee todos
+    todos = requests.get(
+        f"{base_url}/todos", params={"userId": employee_id}
+    ).json()
+
+    done_tasks = [task for task in todos if task.get("completed")]
+    total = len(todos)
+    done = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({done}/{total}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+        
